3fa5244649957c7cd3aff363f04f52d243b68ac2
Yevhenii Serdiuk <yevheniiserdiuk@gmail.com>
2022-05-18 23:21:24 +0300

    add try catch

--- src/TypeUtilities.SourceGenerators/Pick/PickSourceGenerator.cs
+++ src/TypeUtilities.SourceGenerators/Pick/PickSourceGenerator.cs
@@ -26,28 +26,37 @@ internal class PickSourceGenerator : IIncrementalGenerator
                 predicate: static (node, _) => node is AttributeSyntax attr && attributeNameRegex.IsMatch(attr.Name.ToString()),
                 transform: static (ctx, ct) =>
                 {
-                    var attributeSyntax = (AttributeSyntax)ctx.Node;
-                    var attributeSymbolInfo = ctx.SemanticModel.GetSymbolInfo(attributeSyntax, ct);
+                    try
+                    {
+                        var attributeSyntax = (AttributeSyntax)ctx.Node;
+                        var attributeSymbolInfo = ctx.SemanticModel.GetSymbolInfo(attributeSyntax, ct);
 
-                    var symbol = attributeSymbolInfo.Symbol ?? attributeSymbolInfo.CandidateSymbols.FirstOrDefault();
+                        var symbol = attributeSymbolInfo.Symbol ?? attributeSymbolInfo.CandidateSymbols.FirstOrDefault();
 
-                    if (symbol is not IMethodSymbol attributeSymbol)
+                        if (symbol is not IMethodSymbol attributeSymbol)
+                            return null;
+
+                        var attributeFullName = attributeSymbol.ContainingType.ToDisplayString();
+
+                        if (attributeFullName != "TypeUtilities.PickAttribute")
+                            return null;
+
+                        if (!attributeSyntax.TryFindParent<TypeDeclarationSyntax>(out var targetTypeSyntax, ct))
+                            return null;
+
+                        var targetTypeSymbolInfo = ctx.SemanticModel.GetDeclaredSymbol(targetTypeSyntax, ct);
+
+                        if (targetTypeSymbolInfo is not INamedTypeSymbol targetTypeSymbol)
+                            return null;
+
+                        return PickTypeConfig.Create(attributeSymbol.ContainingType, targetTypeSymbol);
+                    }
+                    catch
+                    {
+                        // TODO: diagnostics?
                         return null;
+                    }
 
-                    var attributeFullName = attributeSymbol.ContainingType.ToDisplayString();
-
-                    if (attributeFullName != "TypeUtilities.PickAttribute")
-                        return null;
-
-                    if (!attributeSyntax.TryFindParent<TypeDeclarationSyntax>(out var targetTypeSyntax, ct))
-                        return null;
-
-                    var targetTypeSymbolInfo = ctx.SemanticModel.GetDeclaredSymbol(targetTypeSyntax, ct);
-
-                    if (targetTypeSymbolInfo is not INamedTypeSymbol targetTypeSymbol)
-                        return null;
-
-                    return PickTypeConfig.Create(attributeSymbol.ContainingType, targetTypeSymbol);
                 })
             .SkipNulls()
             .WithComparer(PickTypeConfig.Comparer)
@@ -55,59 +64,65 @@ internal class PickSourceGenerator : IIncrementalGenerator
 
         // Generate the source using the compilation and enums
         context.RegisterSourceOutput(attributes, static (context, tuple) => {
-            var config = tuple.Left!;
-            var types = tuple.Right!;
-
-            // TODO: support base class members
-            var pickedMembers = config.Fields
-                .Select(f => config.Source.GetMember(f, config.IncludeBaseTypes, context.CancellationToken));
-
-            var targetTypeSyntax = types[config.Target.ToDisplayString()];
-
-            var sourceBuilder = new StringBuilder();
-
-            if (!config.Target.ContainingNamespace.IsGlobalNamespace)
+            try
             {
-                var @namespace = config.Target.ContainingNamespace.ToDisplayString();
+                //TODO: add global try catch with diagnostics
+                var config = tuple.Left!;
+                var types = tuple.Right!;
 
-                sourceBuilder.AppendLine($"namespace {@namespace};\n");
-            }
+                // TODO: support base class members
+                var pickedMembers = config.Fields
+                    .Select(f => config.Source.GetMember(f, config.IncludeBaseTypes, context.CancellationToken));
 
-            // TODO: proper conversion
-            var accessability = config.Target.DeclaredAccessibility.ToString().ToLower();
-            var targetName = config.Target.Name;
+                var targetTypeSyntax = types[config.Target.ToDisplayString()];
 
-            sourceBuilder.AppendLine($"{targetTypeSyntax.Modifiers} {targetTypeSyntax.Keyword} {targetName}");
-            sourceBuilder.AppendLine("{");
+                var sourceBuilder = new StringBuilder();
 
-            foreach (var member in pickedMembers)
-            {
-                if (member is null)
-                    continue;
-
-                var accessibility = member.DeclaredAccessibility.ToString().ToLower();
-
-                if (member is IPropertySymbol prop)
+                if (!config.Target.ContainingNamespace.IsGlobalNamespace)
                 {
-                    var propType = prop.Type.ToDisplayString();
-                    var propName = prop.Name;
+                    var @namespace = config.Target.ContainingNamespace.ToDisplayString();
 
-                    sourceBuilder.AppendLine($"    {accessibility} {propType} {propName}" + " { get; set; }");
-                    continue;
+                    sourceBuilder.AppendLine($"namespace {@namespace};\n");
                 }
 
-                if (member is IFieldSymbol field)
+                // TODO: proper conversion
+                var accessability = config.Target.DeclaredAccessibility.ToString().ToLower();
+                var targetName = config.Target.Name;
+
+                sourceBuilder.AppendLine($"{targetTypeSyntax.Modifiers} {targetTypeSyntax.Keyword} {targetName}");
+                sourceBuilder.AppendLine("{");
+
+                foreach (var member in pickedMembers)
                 {
-                    var propType = field.Type.ToDisplayString();
-                    var propName = field.Name;
+                    if (member is null)
+                        continue;
 
-                    sourceBuilder.AppendLine($"    {accessibility} {propType} {propName};");
+                    var accessibility = member.DeclaredAccessibility.ToString().ToLower();
+
+                    if (member is IPropertySymbol prop)
+                    {
+                        var propType = prop.Type.ToDisplayString();
+                        var propName = prop.Name;
+
+                        sourceBuilder.AppendLine($"    {accessibility} {propType} {propName}" + " { get; set; }");
+                        continue;
+                    }
+
+                    if (member is IFieldSymbol field)
+                    {
+                        var propType = field.Type.ToDisplayString();
+                        var propName = field.Name;
+
+                        sourceBuilder.AppendLine($"    {accessibility} {propType} {propName};");
+                    }
                 }
+
+                sourceBuilder.AppendLine("}");
+
+                context.AddSource($"{targetName}.g.cs", SourceText.From(sourceBuilder.ToString(), Encoding.Unicode));
             }
+            catch { /* TODO: diagnostics? */ }
 
-            sourceBuilder.AppendLine("}");
-
-            context.AddSource($"{targetName}.g.cs", SourceText.From(sourceBuilder.ToString(), Encoding.Unicode));
         });
     }
 }
